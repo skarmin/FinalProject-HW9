e52765e695458983fed866a19bbb0f781abea0db
Sam Karmin <skarmin@umich.edu>
2017-04-09 22:53:12 -0400

    Completed Project Plan

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -26,6 +26,33 @@ import sqlite3
 
 
 # Write your test cases here.
+class CacheTesting(unittest.TestCase):
+	def test_movie_cache(self):
+		cache_file = open("SI206_FinalProject_cache.json","r").read()
+		self.assertTrue("Movies" in cache_file)
+	def test_twitter_cache(self):
+		cache_file = open("SI206_FinalProject_cache.json","r").read()
+		self.assertTrue("Tweets" in cache_file)
+class MovieTesting(unittest.TestCase):
+	def test_Movie_str_method(self):
+		movie_instance = Movie(movie_dic)
+		self.assertEqual(type(movie_instance.__str__(), str))
+	def test_get_actors_method(self):
+		movie_instance = Movie(movie_instance)
+		self.assertEqual(type(movie_instance.get_movie_actors()), list))
+	def test_get_actors_method2(self):
+		movie_instance = Movie(movie_instance)
+		actors = movie_instance.get_movie_actors()[0]
+		self.assertEqual(type(actors), list))
+	def test_title_of_movie(self):
+		movie_instance = Movie(movie_instance)
+		self.assertEqual(type(movie_instance.tittle), str)
+	def test_movie_tweets(self):
+ 		self.assertEqual(type(movie_tweets),type([]))
+ 	def test_actor(self):
+ 		self.assertEqual(type(get_movie_actors[18]),type({"hi":3}))
+
+
 
 
 ## Remember to invoke all your tests...
