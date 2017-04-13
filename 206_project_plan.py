aedd51319f9bf4c8bfea34cc1f07ef60998918b2
Sam Karmin <skarmin@umich.edu>
2017-04-12 22:50:48 -0400

    Properly retrieving moving info

--- 206_project_plan.py
+++ 206_project_plan.py
@@ -9,9 +9,9 @@ import itertools
 import re
 import json
 import sqlite3
+import requests
 
 #### TWEEPY SETUP CODE:
-# Authentication information should be in a twitter_info file...
 consumer_key = twitter_info.consumer_key
 consumer_secret = twitter_info.consumer_secret
 access_token = twitter_info.access_token
@@ -26,7 +26,7 @@ api = tweepy.API(auth, parser=tweepy.parsers.JSONParser())
 
 #CACHING SYSTEM
 
-CACHE_FNAME = "I206_FinalProject_cache.json"
+CACHE_FNAME = "206_FinalProject_cache.json"
 # Put the rest of your caching setup here:
 try: 
 	cache_file = open(CACHE_FNAME, 'r')
@@ -39,87 +39,85 @@ except:
 
 #CREATE MOVIE CLASS
 class Movie():
-	def __init__(self, movie_dict):
-        self.movie_dict = movie_dict   #confused about this part
-        self.movie_title = movie.title
-        self.movie_director = movie_director
-        self.movie_IMBD = movie_IMBD
+	def __init__(self, movie_title):
+		self.movie_title = movie_title
+        # self.movie_director = movie_director
+        # self.movie_IMBD = movie_IMBD
 
+	def __str__(self):
+		return "{} is directed by {} and received a an IMBD rating of {}".format(self.movie.title, self.movie_director, self.movie_IMBD)
 
-    def __str__(self):
-        return "{} is directed by {} and received a an IMBD rating of {}".format(self.movie.title, self.movie_director, self.movie_IMBD)
-
-	def get_movie_actors(self):
+	def get_movie_info(self):
 		base_url = 'http://www.omdbapi.com/'
-		full_url = requestURL(base_url, params = {"t": self.movie_title})
+		full_url = requests.get(base_url, params = {"t": self.movie_title}).text
 
 		if self.movie_title in CACHE_FNAME:
-			omdb_response_text = CACHE_DICTION[full_url]
+			omdb_response_text = CACHE_DICTION[self.movie_title] #Why response_text vs just response
 		else:
-			omdb_response = requests.get(full_url)
-			CACHE_DICTION[full_url] = omdb_response.text
-			omdb_response_text = omdb_response.text
-
+			CACHE_DICTION[self.movie_title] = full_url
+			omdb_response_text = full_url
 			cache_file = open(CACHE_FNAME, 'w')
 			cache_file.write(json.dumps(CACHE_DICTION))
 			cache_file.close
-		return json.loads(omdb_response_text) #DOES THIS RETURN A DICTIOANRY?? IT SHOULD! Also, need to just grab the INFO aobut actors,not the entire movie!
+		movie_dict = json.loads(omdb_response_text)
 		
+		actors = movie_dict['Actors']
+		director = movie_dict['Director']
+		rating = movie_dict['Ratings'][0]['Value']
+		return actors, director, rating
+	
+	def get_twitter_info(self, actors):
+		actor_tweets = {}
+		for actor in actors:
+			tweets = api.search(actor)
+			actor_tweets[actor] = tweets
+
+		return actor_tweets
 
 
 
-		##########THIS EXAMPLE WORKS, DOES IT WORK FOR THIS METHOD AS WELL######
-		# if full_url in omdb_cache_diction:
-		# 	omdb_response_text = omdb_cache_diction[full_url]
-		# else:
-		# 	omdb_response = requests.get(full_url)
-		# 	omdb_cache_diction[full_url] = omdb_response.text
-		# 	omdb_response_text = omdb_response.text
-
-		# 	cache_file = open(omdb_cached_data, 'w')
-		# 	cache_file.write(json.dumps(omdb_cache_diction))
-		# 	cache_file.close
-		# return json.loads(omdb_response_text)
+Miracle = Movie("Miracle On Ice")
+movie_info = Miracle.get_movie_info()
+print (Miracle.get_twitter_info(movie_info[0]))
 
 
-def requestURL(base_url, params = {}):
-	r = requests.Request(method = 'GET', url = base_url, params = params)
-	prep = r.prepare()
-	return prep.url
+#GET TWitter info
 
 
 
 
 # Write your test cases here.
-class CacheTesting(unittest.TestCase):
-	def test_movie_cache(self):
-		cache_file = open("SI206_FinalProject_cache.json","r").read()
-		self.assertTrue("Movies" in cache_file)
-	def test_twitter_cache(self):
-		cache_file = open("SI206_FinalProject_cache.json","r").read()
-		self.assertTrue("Tweets" in cache_file)
-class MovieTesting(unittest.TestCase):
-	def test_Movie_str_method(self):
-		movie_instance = Movie(movie_dic)
-		self.assertEqual(type(movie_instance.__str__(), str))
-	def test_get_actors_method(self):
-		movie_instance = Movie(movie_instance)
-		self.assertEqual(type(movie_instance.get_movie_actors()), list))
-	def test_get_actors_method2(self):
-		movie_instance = Movie(movie_instance)
-		actors = movie_instance.get_movie_actors()[0]
-		self.assertEqual(type(actors), list))
-	def test_title_of_movie(self):
-		movie_instance = Movie(movie_instance)
-		self.assertEqual(type(movie_instance.tittle), str)
-	def test_movie_tweets(self):
- 		self.assertEqual(type(movie_tweets),type([]))
- 	def test_actor(self):
- 		self.assertEqual(type(get_movie_actors[18]),type({"hi":3}))
+# class CacheTesting(unittest.TestCase):
+# 	def test_movie_cache(self):
+# 		cache_file = open("SI206_FinalProject_cache.json","r").read()
+# 		self.assertTrue("Movies" in cache_file)
+# 	def test_twitter_cache(self):
+# 		cache_file = open("SI206_FinalProject_cache.json","r").read()
+# 		self.assertTrue("Tweets" in cache_file)
+# class MovieTesting(unittest.TestCase):
+# 	def test_Movie_str_method(self):
+# 		movie_instance = Movie(movie_dic)
+# 		self.assertEqual(type(movie_instance.__str__(), str))
+# 	def test_get_actors_method(self):
+# 		movie_instance = Movie(movie_instance)
+# 		self.assertEqual(type(movie_instance.get_movie_actors()), list))
+# 	def test_get_actors_method2(self):
+# 		movie_instance = Movie(movie_instance)
+# 		actors = movie_instance.get_movie_actors()[0]
+# 		self.assertEqual(type(actors), list))
+# 	def test_title_of_movie(self):
+# 		movie_instance = Movie(movie_instance)
+# 		self.assertEqual(type(movie_instance.tittle), str)
+# 	def test_movie_tweets(self):
+#  		self.assertEqual(type(movie_tweets),type([]))
+#  	def test_actor(self):
+#  		self.assertEqual(type(get_movie_actors[18]),type({"hi":3}))
 
 
 
 
 ## Remember to invoke all your tests...
-if __name__ == "__main__":
-	unittest.main(verbosity=2)
+# if __name__ == "__main__":
+# 	unittest.main(verbosity=2)
+
+
